70cb624909694fd9991013f093c1721674f88996
Joe <modulusman@gmail.com>
2019-05-13 21:25:15 -0700

    sets up crud routes, updates controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # /about will show in url, maps to pages controller, about method
   get 'about' => 'pages#about'
+  # setting the posts as a resource enables crud routes for the posts controller
+  resources :posts
+  # route to landing page root
   root 'posts#index'
 end
