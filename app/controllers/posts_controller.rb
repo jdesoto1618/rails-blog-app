cebc675109ff3202c3a57470e1e6f881ce6a4d85
Joe <modulusman@gmail.com>
2019-05-16 11:02:59 -0700

    new post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,21 @@ class PostsController < ApplicationController
   end
 
   def create
+    # Post.new references the model. post_params references private method
+    @post = Post.new(post_params)
+    # save a new post once it's created
+    @post.save
+    # show the new post after it's created and saved
+    redirect_to @post
+  end
 
+  def show
+    # show a post after it's created. pass in the id of the post from the url
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
